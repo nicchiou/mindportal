1f1cfdf003faf477735fd2275d02090f907d5cc9
xsmallninja <xsmallninja@gmail.com>
2021-11-13 18:47:48 -0600

    NC: Put CSP and feature extraction inside the cross-validation loop. Enable RBF SVM classifier training.

--- base_classifier/train_base_clf.py
+++ base_classifier/train_base_clf.py
@@ -1,58 +1,20 @@
 import argparse
+import copy
 import json
 import os
 
 import numpy as np
 import pandas as pd
 from sklearn import svm
-from sklearn.decomposition import PCA
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
-from sklearn.feature_selection import SelectFromModel, mutual_info_classif
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
-from tqdm import tqdm
-
 from utils import constants
 
-
-def combine_motor_LR(x):
-    """
-    Combines trial types for left motor responses and right motor responses.
-
-    0 represents left and 1 represents right.
-    """
-    if x in [2, 4, 6, 8]:
-        return 0
-    elif x in [1, 3, 5, 7]:
-        return 1
-    else:
-        return np.nan
-
-
-def stim_modality_motor_LR_labels(x):
-    """
-    Combines trial types such that the problem is a multi-class classification
-    problem with the goal to predict both the stimulus modality (visual or
-    auditory) as well as the motor response (L/R).
-
-    0: visual, right
-    1: visual, left
-    2: auditory, right
-    3: auditory, left
-
-    Returns np.nan for unsupported trial types.
-    """
-    if x in [1, 5]:
-        return 0
-    elif x in [2, 6]:
-        return 1
-    elif x in [3, 7]:
-        return 2
-    elif x in [4, 8]:
-        return 3
-    else:
-        return np.nan
+from csp_lib.CSP import run_csp_filtering
+from csp_lib.data_splits import learn_eval_split_csp, learn_eval_split_non_csp
+from csp_lib.features import select_features
 
 
 def train_eval_clf(args, X_train, y_train, X_valid, y_valid, X_test, y_test):
@@ -67,9 +29,11 @@ def train_eval_clf(args, X_train, y_train, X_valid, y_valid, X_test, y_test):
     if args.clf_type == 'SVM':
         clf = svm.LinearSVC(penalty=args.penalty, loss=args.loss, C=args.C,
                             max_iter=args.max_iter, random_state=args.seed,
-                            dual=False if args.penalty == 'l1' and \
-                                          args.loss == 'squared_hinge' \
-                                       else True)
+                            dual=False if args.penalty == 'l1' and
+                            args.loss == 'squared_hinge' else True)
+    elif args.clf_type == 'RBF_SVM':
+        clf = svm.SVC(C=args.C, max_iter=args.max_iter,
+                      random_state=args.seed)
     elif args.clf_type == 'RF':
         clf = RandomForestClassifier(n_estimators=200, criterion='entropy',
                                      max_depth=args.max_depth,
@@ -81,10 +45,10 @@ def train_eval_clf(args, X_train, y_train, X_valid, y_valid, X_test, y_test):
     clf.fit(X_train, y_train)
 
     trial_results = dict()
-    
+
     y_pred = clf.predict(X_train)
     class_rep = classification_report(y_train, y_pred, output_dict=True,
-                                      zero_division='warn')
+                                      zero_division=0)
     trial_results['Train Accuracy'] = class_rep['accuracy']
     trial_results['Train Precision'] = class_rep['macro avg']['precision']
     trial_results['Train Recall'] = class_rep['macro avg']['recall']
@@ -92,7 +56,7 @@ def train_eval_clf(args, X_train, y_train, X_valid, y_valid, X_test, y_test):
 
     y_pred = clf.predict(X_valid)
     class_rep = classification_report(y_valid, y_pred, output_dict=True,
-                                      zero_division='warn')
+                                      zero_division=0)
     trial_results['Valid Accuracy'] = class_rep['accuracy']
     trial_results['Valid Precision'] = class_rep['macro avg']['precision']
     trial_results['Valid Recall'] = class_rep['macro avg']['recall']
@@ -100,7 +64,7 @@ def train_eval_clf(args, X_train, y_train, X_valid, y_valid, X_test, y_test):
 
     y_pred = clf.predict(X_test)
     class_rep = classification_report(y_test, y_pred, output_dict=True,
-                                      zero_division='warn')
+                                      zero_division=0)
     trial_results['Test Accuracy'] = class_rep['accuracy']
     trial_results['Test Precision'] = class_rep['macro avg']['precision']
     trial_results['Test Recall'] = class_rep['macro avg']['recall']
@@ -117,20 +81,30 @@ montages = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--exp_dir', type=str, required=True)
     parser.add_argument('--clf_type', type=str, default='SVM',
-                        help='options are: SVM, RF')
+                        help='options are: SVM, RBF_SVM, RF')
     parser.add_argument('--window', type=str, default='all',
-                        help='options include all, rt, pre_stim, init, ' \
-                        'pre_rt, post_rt')
-    parser.add_argument('--classification_type', type=str, default='motor_LR',
-                        help='options include motor_LR (motor response) and ' \
-                        'motor_stim_LR (stimulus modality and motor response')
+                        help='options include 0-7')
+    parser.add_argument('--unfilt', action='store_true')
+    parser.add_argument('--csp', action='store_true')
+    parser.add_argument('--log_variance_feats', action='store_true',
+                        help='indicates whether to use CSP-derived '
+                        'log-variance features instead of band-power features')
+    parser.add_argument('--n_filters', type=int, default=16,
+                        help='number of CSP filters used to generate features')
+    parser.add_argument('--classification_task', type=str, default='motor_LR',
+                        help='options include motor_LR (motor response), '
+                        'stim_motor (stimulus modality and motor response) '
+                        'and response_stim (response modality, stimulus '
+                        'modality, and response polarity).')
+    parser.add_argument('--anchor', type=str, default='pc',
+                        help='pre-cue (pc) or response stimulus (rs)')
     parser.add_argument('--selection_method', type=str, default='None',
-                        help='options are: PCA, MI, tree, linear, RFE, SFS, ' \
+                        help='options are: PCA, MI, tree, linear, RFE, SFS, '
                         'None')
     parser.add_argument('--selected_dir', type=str)
     parser.add_argument('--n_components', type=int, default=100,
@@ -152,141 +126,101 @@ if __name__ == '__main__':
     parser.add_argument('--sampler_seed', type=int, default=8)
     parser.add_argument('--seed', type=int, default=8)
     parser.add_argument('--n_splits', type=int, default=5, help='cv splits')
+    parser.add_argument('--randomize_trials', action='store_true')
 
     args = parser.parse_args()
-    model_name = 'linear_svm' if args.clf_type == 'SVM' else 'random_forest'
-    exp_dir = os.path.join(constants.RESULTS_DIR, args.classification_type,
-                           'baseline', model_name, args.exp_dir)
+    if args.clf_type == 'SVM':
+        model_name = 'linear_svm'
+    elif args.clf_type == 'RBF_SVM':
+        model_name = 'rbf_svm'
+    else:
+        model_name = 'random_forest'
+    exp_dir = os.path.join(constants.RESULTS_DIR, args.classification_task,
+                           'csp_baseline' if args.csp else 'baseline',
+                           model_name, args.anchor, args.exp_dir)
     os.makedirs(exp_dir, exist_ok=True)
-    with open(os.path.join(exp_dir, f'args.json'), 'w') as f:
+    with open(os.path.join(exp_dir, 'args.json'), 'w') as f:
         json.dump(args.__dict__, f, indent=4)
-    
+
     results_df = pd.DataFrame()
 
-    # Input features (not yet selected)
-    df = pd.read_parquet(
-        os.path.join(constants.BANDPOWER_DIR,
-                     f'{args.window}_simple_bandpower_features.parquet'))
-    
+    # Input file (if csp, contains signals; else, contains features)
+    if args.csp:
+        in_dir = os.path.join(constants.PHASE_DATA_DIR, args.anchor)
+        if args.unfilt:
+            if args.window == 'all':
+                fname = 'phase_all_single_trial.parquet'
+            else:
+                fname = f'phase_win{args.window}_single_trial.parquet'
+        else:
+            if args.window == 'all':
+                fname = 'phase_all_filt_chan.parquet'
+            else:
+                fname = f'phase_win{args.window}_filt_chan.parquet'
+    else:
+        in_dir = os.path.join(constants.BANDPOWER_DIR, args.anchor)
+        if args.window == 'all':
+            fname = 'all_simple_bandpower_features.parquet'
+        else:
+            fname = f'win{args.window}_simple_bandpower_features.parquet'
+    df = pd.read_parquet(os.path.join(in_dir, fname))
+
     subjects = df['subject_id'].unique()
 
-    for subject_id in tqdm(subjects):
+    for subject_id in subjects:
         for montage in montages:
-        
+
             trial_results = dict()
             trial_results['subject_id'] = subject_id
             trial_results['montage'] = montage
+            if args.csp:
+                trial_results['n_filters'] = args.n_filters
 
-            subset = df[(df['subject_id'] == str(subject_id)) & \
+            subset = df[(df['subject_id'] == str(subject_id)) &
                         (df['montage'] == montage)].copy().reset_index(
                             drop=True)
-            feat_cols = np.array([c for c in subset.columns if 'ph' in c])
-            discrete_feats = ((np.core.defchararray.find(
-                                    feat_cols, 'samp_gt_zero') != -1) | 
-                              (np.core.defchararray.find(
-                                    feat_cols, 'zero_cross') != -1))
-            # Combine trial types
-            if args.classification_type == 'motor_LR':
-                subset.loc[:, 'label'] = subset.loc[:, 'trial_type'].apply(
-                    combine_motor_LR)
-            elif args.classification_type == 'motor_stim_LR':
-                subset.loc[:, 'label'] = subset.loc[:, 'trial_type'].apply(
-                    stim_modality_motor_LR_labels)
-            subset = subset.dropna(
-                axis=0, subset=['label']).copy().reset_index(drop=True)
-            
-            # Remove features that are based on zeroed channels across all trials
-            subset = subset.loc[:, \
-                subset.isnull().sum(axis=0) != subset.shape[0]]
 
-            # Keep track of features with zeroed channels for specific trials
-            na_mask = subset.isna().any(axis=0).values
-            na_cols = list(subset.columns[na_mask])
-            feat_cols = np.array([c for c in subset.columns if 'ph' in c])
-            discrete_feats = ((np.core.defchararray.find(
-                                    feat_cols, 'samp_gt_zero') != -1) | 
-                              (np.core.defchararray.find(
-                                    feat_cols, 'zero_cross') != -1))
-            subset = subset.fillna(0)
-
-            # Train-test split
-            X = subset[feat_cols].values
-            y = subset['label'].values
-            X_learn, X_test, y_learn, y_test = train_test_split(
-                X, y, test_size=0.2, stratify=y,
-                random_state=args.sampler_seed)
-
-            if args.selection_method in ['RFE', 'SFS']:
-                # Load selected features
-                selected_feat_idx = np.load(
-                    os.path.join(args.selected_dir,
-                                f'{subject_id}_{montage}_support.npy'))
-                trial_results['num_features'] = \
-                    np.count_nonzero(selected_feat_idx)
-                X_learn = X_learn[:, selected_feat_idx]
-                X_test = X_test[:, selected_feat_idx]
-            elif args.selection_method == 'PCA':
-                pca = PCA(n_components=args.n_components)
-                pca.fit(X_learn)
-                exp_var = pca.explained_variance_ratio_
-                trial_results['num_components'] = args.n_components
-                trial_results['explained_variance'] = exp_var
-                X_learn = pca.transform(X_learn)
-                X_test = pca.transform(X_test)
-            elif args.selection_method == 'MI':
-                mi_scores = mutual_info_classif(X_learn, y_learn,
-                    discrete_features=discrete_feats,
-                    n_neighbors=args.n_neighbors)
-                mi_score_selected_index = np.where(
-                    mi_scores > args.score_threshold)[0]
-                trial_results['n_neighbors'] = args.n_neighbors
-                trial_results['score_treshold'] = args.score_threshold
-                X_learn = X_learn[:, mi_score_selected_index]
-                X_test = X_test[:, mi_score_selected_index]
-            elif args.selection_method == 'tree':
-                clf = ExtraTreesClassifier(n_estimators=args.n_estimators)
-                clf = clf.fit(X_learn, y_learn)
-                selector = SelectFromModel(clf, max_features=args.n_features,
-                                           prefit=True)
-                trial_results['n_estimators'] = args.n_estimators
-                trial_results['max_features'] = args.n_features
-                X_learn = selector.transform(X_learn)
-                X_test = selector.transform(X_test)
-                trial_results['n_features'] = X_learn.shape[1]
-            elif args.selection_method == 'linear':
-                scaler = StandardScaler()
-                scaler.fit(X_learn)
-                X_learn = scaler.transform(X_learn)
-                clf = svm.LinearSVC(C=1, penalty='l1', dual=False,
-                                    max_iter=10000)
-                clf = clf.fit(X_learn, y_learn)
-                selector = SelectFromModel(clf, max_features=args.n_features,
-                                           prefit=True)
-                trial_results['max_features'] = args.n_features
-                X_learn = selector.transform(X_learn)
-                X_test = selector.transform(X_test)
-                trial_results['n_features'] = X_learn.shape[1]
-            elif args.selection_method == 'None':
-                trial_results['n_features'] = X_learn.shape[1]
+            if args.csp:
+                X_learn, X_eval, y_learn, y_eval, X, y, learn_idx, eval_idx = \
+                    learn_eval_split_csp(args, subset)
             else:
-                raise NotImplementedError(
-                    'Feature selection method not implemented!')
-                
+                X_learn, X_eval, y_learn, y_eval = \
+                    learn_eval_split_non_csp(args, subset)
+
             # Stratified K-fold cross-validation
             skf = StratifiedKFold(n_splits=args.n_splits, shuffle=True,
                                   random_state=args.sampler_seed)
             for idx, (train_idx, valid_idx) in enumerate(
                     skf.split(X_learn, y_learn)):
                 trial_results['idx'] = idx
+
                 X_train, X_valid = X_learn[train_idx], X_learn[valid_idx]
                 y_train, y_valid = y_learn[train_idx], y_learn[valid_idx]
+                X_test, y_test = \
+                    copy.deepcopy(X_eval), copy.deepcopy(y_eval)
 
-                eval_results = train_eval_clf(args,
-                    X_train, y_train, X_valid, y_valid, X_test, y_test)
+                if args.csp:
+                    X_train, y_train, X_valid, y_valid, X_test, y_test = \
+                        run_csp_filtering(train_idx, valid_idx,
+                                          learn_idx, eval_idx,
+                                          X_train, y_train,
+                                          X, y, subset, args)
+                # Select features if applicable
+                if args.selection_method == 'None':
+                    trial_results['n_features'] = X_train.shape[1]
+                else:
+                    X_train, X_valid, X_test, trial_results = \
+                        select_features(args, trial_results,
+                                        X_train, y_train, X_valid, X_test)
+
+                # Train and evaluate the model
+                eval_results = train_eval_clf(
+                    args, X_train, y_train, X_valid, y_valid, X_test, y_test)
                 trial_results.update(eval_results)
 
-                results_df = results_df.append(trial_results, ignore_index=True)
-        
+                results_df = results_df.append(trial_results,
+                                               ignore_index=True)
+
     results_df.to_parquet(
         os.path.join(exp_dir, f'{args.selection_method}_results.parquet'),
         index=False)
