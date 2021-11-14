0acea4ca7ab57d4036a8232ccccef398781704c4
xsmallninja <xsmallninja@gmail.com>
2021-11-13 18:57:42 -0600

    NC: Modularize filtering zeros from channels and adding the capability to save individual window phase data.

--- preprocessing/data_cleaning/filter_zero_channels.py
+++ preprocessing/data_cleaning/filter_zero_channels.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 from utils import constants
 
 ###############################################################################
-# 
+#
 # Filters the channels with all zero values and replaces them with NaN in the
 # DataFrame cell for each frequency band.
 #
 # Author: Nicole Chiou
-#  
+#
 ###############################################################################
 
 
@@ -24,17 +24,21 @@ if __name__ == '__main__':
                         default=os.path.join(constants.PYTHON_DIR, 'ac_dc_ph'))
     parser.add_argument('--anchor', type=str, default='pc',
                         help='pre-cue (pc) or response stimulus (rs)')
+    parser.add_argument('--bandpass_only', action='store_true')
     parser.add_argument('-l', '--input_dirs', nargs='+',
                         default=['pc00-04avg', 'pc00-08avg', 'pc00-13avg'],
                         help='directories to process from')
     parser.add_argument('--filter_zeros', action='store_true')
     parser.add_argument('--concat_unfilt', action='store_true')
     parser.add_argument('--concat_filt', action='store_true')
-   
+    parser.add_argument('--save_phase_data', action='store_true')
+
     args = parser.parse_args()
 
     baseline_dirs = [f'{d}_{args.anchor}' for d in args.input_dirs]
-    out_dir = os.path.join(args.output_dir, args.anchor)
+    out_dir = os.path.join(
+        args.output_dir, args.anchor,
+        'bandpass_only' if args.bandpass_only else 'rect_lowpass')
     os.makedirs(out_dir, exist_ok=True)
 
     # Filter zero channels from "pc00- all single trial" files
@@ -56,10 +60,19 @@ if __name__ == '__main__':
             if '00-04' in d:
                 pc04 = pd.read_parquet(
                     os.path.join(out_dir, f'{d}_all_single_trial.parquet'))
+            elif '04-07' in d:
+                pc04 = pd.read_parquet(
+                    os.path.join(out_dir, f'{d}_all_single_trial.parquet'))
             elif '00-08' in d:
                 pc08 = pd.read_parquet(
                     os.path.join(out_dir, f'{d}_all_single_trial.parquet'))
+            elif '08-13' in d:
+                pc08 = pd.read_parquet(
+                    os.path.join(out_dir, f'{d}_all_single_trial.parquet'))
             elif '00-13' in d:
+                pc13 = pd.read_parquet(
+                    os.path.join(out_dir, f'{d}_all_single_trial.parquet'))
+            elif '13-20' in d:
                 pc13 = pd.read_parquet(
                     os.path.join(out_dir, f'{d}_all_single_trial.parquet'))
 
@@ -71,17 +84,64 @@ if __name__ == '__main__':
         all_data.to_parquet(
             os.path.join(out_dir, 'all_single_trial.parquet'), index=False)
 
-    
+        # Filter phase features only
+        if args.save_phase_data:
+            data_dir = os.path.join(constants.PHASE_DATA_DIR, args.anchor)
+            ph_data = all_data[
+                [c for c in all_data.columns
+                 if ('ac_' not in c) and ('dc_' not in c)]]
+            ph_data.to_parquet(
+                os.path.join(data_dir, 'phase_single_trial.parquet'),
+                index=False)
+
+            # Isolate 500 ms windows
+            ph_data = ph_data.rename({'trial_num_04': 'trial_num',
+                                      'subject_id_04': 'subject_id',
+                                      'trial_type_04': 'trial_type',
+                                      'montage_04': 'montage'}, axis=1)
+            ph_data = ph_data.drop(['trial_num_08', 'subject_id_08',
+                                    'trial_type_08', 'montage_08',
+                                    'trial_num_13', 'subject_id_13',
+                                    'trial_type_13', 'montage_13'], axis=1)
+
+            # Utilize only the full signal
+            ph_all = ph_data[
+                [c for c in ph_data.columns
+                 if len(c.split('_')) == 4 and 'ph_' in c]
+                ['trial_num', 'subject_id', 'trial_type', 'montage']]
+            ph_all.to_parquet(
+                os.path.join(data_dir, 'phase_all_single_trial.parquet'),
+                index=False)
+
+            # Filter windows of 500 ms
+            for i in tqdm(range(8)):
+                ph_win = ph_data[
+                    [c for c in ph_data.columns if f'win{i}' in c]
+                    ['trial_num', 'subject_id', 'trial_type', 'montage']]
+                ph_win.to_parquet(
+                    os.path.join(data_dir,
+                                 f'phase_win{i}_single_trial.parquet'),
+                    index=False)
+
     # Aggregate over all frequencies' filtered channel data
     if args.concat_filt:
         for d in baseline_dirs:
             if '00-04' in d:
                 pc04 = pd.read_parquet(
                     os.path.join(out_dir, f'{d}_filt_chan.parquet'))
+            elif '04-07' in d:
+                pc04 = pd.read_parquet(
+                    os.path.join(out_dir, f'{d}_filt_chan.parquet'))
             elif '00-08' in d:
                 pc08 = pd.read_parquet(
                     os.path.join(out_dir, f'{d}_filt_chan.parquet'))
+            elif '08-13' in d:
+                pc08 = pd.read_parquet(
+                    os.path.join(out_dir, f'{d}_filt_chan.parquet'))
             elif '00-13' in d:
+                pc13 = pd.read_parquet(
+                    os.path.join(out_dir, f'{d}_filt_chan.parquet'))
+            elif '13-20' in d:
                 pc13 = pd.read_parquet(
                     os.path.join(out_dir, f'{d}_filt_chan.parquet'))
 
@@ -92,3 +152,41 @@ if __name__ == '__main__':
 
         all_data.to_parquet(
             os.path.join(out_dir, 'all_filt_chan.parquet'), index=False)
+
+        # Filter phase features only
+        if args.save_phase_data:
+            data_dir = os.path.join(constants.PHASE_DATA_DIR, args.anchor)
+            ph_data = all_data[
+                [c for c in all_data.columns
+                 if ('ac_' not in c) and ('dc_' not in c)]]
+            ph_data.to_parquet(
+                os.path.join(data_dir, 'phase_filt_chan.parquet'),
+                index=False)
+
+            # Isolate 500 ms windows
+            ph_data = ph_data.rename({'trial_num_04': 'trial_num',
+                                      'subject_id_04': 'subject_id',
+                                      'trial_type_04': 'trial_type',
+                                      'montage_04': 'montage'}, axis=1)
+            ph_data = ph_data.drop(['trial_num_08', 'subject_id_08',
+                                    'trial_type_08', 'montage_08',
+                                    'trial_num_13', 'subject_id_13',
+                                    'trial_type_13', 'montage_13'], axis=1)
+
+            # Utilize only the full signal
+            ph_all = ph_data[
+                [c for c in ph_data.columns
+                 if len(c.split('_')) == 4 and 'ph_' in c] +
+                ['trial_num', 'subject_id', 'trial_type', 'montage']]
+            ph_all.to_parquet(
+                os.path.join(data_dir, 'phase_all_filt_chan.parquet'),
+                index=False)
+
+            # Filter windows of 500 ms
+            for i in tqdm(range(8)):
+                ph_win = ph_data[
+                    [c for c in ph_data.columns if f'win{i}' in c] +
+                    ['trial_num', 'subject_id', 'trial_type', 'montage']]
+                ph_win.to_parquet(
+                    os.path.join(data_dir, f'phase_win{i}_filt_chan.parquet'),
+                    index=False)
