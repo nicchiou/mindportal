cc3b86bb2646c5b4b11f163fade4bc2ae2ac614e
xsmallninja <xsmallninja@gmail.com>
2022-01-29 09:49:22 -0600

    NC: Added more subjects to the subset of subjects to evaluate (based on number of viable channels).

--- libraries/utils/utils/constants.py
+++ libraries/utils/utils/constants.py
@@ -18,6 +18,6 @@ RESULTS_DIR = os.path.join(SHARED_DIR, 'mindportal')
 
 SUBJECT_IDS = ['127', '130', '146', '149', '150', '151', '152', '153', '154',
                '155', '157', '505', '516', '527', '534']
-SUBSET_SUBJECT_IDS = ['154', '534']
+SUBSET_SUBJECT_IDS = ['154', '534', '152', '527', '505', '127']
 MONTAGES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 PAIRED_MONTAGES = ['A', 'B', 'C', 'D']
